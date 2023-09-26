f26d350d16df98f6611fa08403e4dbd369784908
GetmanYri <noglum@yandex.ru>
2023-09-26 11:36:15 +0300

    Fixed name of file task9

--- mod2_topic2/src/main/java/getman/homework/task9/RequestCounter.java
+++ mod2_topic2/src/main/java/getman/homework/task9/RequestCounter.java
@@ -3,51 +3,52 @@ package getman.homework.task9;
 
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
-import java.util.Date;
 //@WebServlet("/task9")
 
-public class SessionCounter extends HttpServlet {
+public class RequestCounter extends HttpServlet {
     private int counter;
-    private final File file = new File("C:/YRI/jd2_homework/jd2_homework/mod2_topic2/src/main/resources/countOfSessions.dat");
+    private final String destFileName = System.getProperty("user.home") + File.separator + "countOfRequests.dat";
+
+    private final File file = new File(destFileName);
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        /*if (!file.exists()) {
+        if (!file.exists()) {
             try {
                 file.createNewFile();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }*/
-        counter=0;
+        }
+        counter = 0;
         counter = getCounter();
 
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println(getCounter());
+        //System.out.println(getCounter());
         counter++;
         setCounter(counter);
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("<html>");
         out.println("<head>");
-        out.println("<title>SessionCounter</title>");
+        out.println("<title>RequestCounter</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Count of session:</h1>");
+        out.println("<h1>Count of request:</h1>");
         out.println(counter);
         out.println("</body>");
         out.println("</html>");
 
     }
+
 
     public int getCounter() {
         int counter = 0;
@@ -74,7 +75,5 @@ public class SessionCounter extends HttpServlet {
             e.printStackTrace();
 
         }
-
-
     }
 }
